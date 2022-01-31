407e8565231f897425b26012b20170fd38eb2058
Bar Perach <barp12@gmail.com>
2022-01-31 20:46:36 +0200

    Parse results

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,70 @@
 use async_std;
 use async_std::net::UdpSocket;
 use async_std::prelude::*;
+use serde::Deserialize;
 use std::str;
 use std::time::{Duration, SystemTime};
 
-struct CuData {}
+#[derive(Deserialize)]
+#[allow(non_snake_case, dead_code)]
+struct CuData {
+    CUIP: String,
+    CUVersion: String,
+    NoUsers: bool,
+    apVer: i32,
+    autoHolidayMode: bool,
+    holiday: bool,
+    ip: String,
+    lat: f64,
+    lon: f64,
+    mac: String,
+    name: String,
+    pin: String,
+    pnpe: bool,
+    port: i32,
+    time: i64,
+    timeStr: String,
+    timeZone: i32,
+    timeZoneName: String,
+    // @Expose
+    // public EnabledFeatures features;
+    // @Expose
+    // public SwitchBeePlace place;
+    // @Expose
+    // public RemoteConnectionType rct;
+    // @Expose
+    // public Role role;
+    // private HashMap<Integer, TimerItem> timersMap;
+    // private Vector<UnitItem> unitItems;
+    // private HashMap<Integer, UnitItem> unitsMap;
+}
 
-async fn collect_responses(socket: UdpSocket) -> std::io::Result<CuData> {
+async fn collect_responses(socket: UdpSocket) -> std::io::Result<Vec<CuData>> {
     let mut buf: [u8; 100000] = [0; 100000];
+
+    let mut results: Vec<CuData> = Vec::new();
 
     let timeout = SystemTime::now()
         .checked_add(Duration::from_secs(5))
         .unwrap();
 
     loop {
-        let now = SystemTime::now();
-        if now.ge(&timeout) {
-            break;
-        }
-        let ret = match socket
-            .recv_from(&mut buf)
-            .timeout(timeout.duration_since(SystemTime::now()).unwrap())
-            .await
-        {
+        let current_dur = match timeout.duration_since(SystemTime::now()) {
+            Ok(val) => val,
+            Err(_) => break,
+        };
+        let ret = match socket.recv_from(&mut buf).timeout(current_dur).await {
             Ok(result) => result,
             Err(_) => break,
         }?;
         println!("data_size: {}, ip: {}", ret.0, ret.1);
         println!("data:");
-        println!("{}", str::from_utf8(&buf[0..ret.0]).unwrap());
+        let str_data = str::from_utf8(&buf[0..ret.0]).unwrap();
+        println!("{}", str_data);
+        results.push(serde_json::from_str(str_data).unwrap());
     }
 
-    Ok(CuData {})
+    Ok(results)
 }
 
 async fn discover_central_units() -> std::io::Result<()> {
