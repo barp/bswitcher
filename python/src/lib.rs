56e25d3ae8500c321cf627c63d0ef793ebfd6c5d
Bar Perach <barp12@gmail.com>
2022-02-10 07:37:49 +0200

    Fix python package depenedencies

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -6,7 +6,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pymodule]
-fn bswitch(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pybswitch(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
