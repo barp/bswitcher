1be54179fbeedd508e49c177c8690476343b09c6
Bar Perach <barp12@gmail.com>
2022-02-11 07:51:33 +0200

    Added more utility functions

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -3,7 +3,7 @@ use async_std::sync::{Arc, Mutex};
 use pyo3::exceptions::PyOSError;
 use pyo3::prelude::*;
 
-use bswitch::api::CombinedError;
+use bswitch::api::{CombinedError, UnitItemOperation};
 use bswitch::protocol::*;
 
 pub struct CombinedErrorWrapper(CombinedError);
@@ -19,6 +19,37 @@ impl From<CombinedErrorWrapper> for PyErr {
 
 #[pyclass(name = "CuClient")]
 pub struct PyCuClient(Arc<Mutex<CuClient>>);
+
+#[pyclass]
+#[derive(Clone)]
+pub struct UnitItem {
+    pub zone: String,
+    pub name: String,
+    pub unit_id: i32,
+    pub value: i32,
+    pub unit_type: i32,
+}
+
+#[pymethods]
+impl UnitItem {
+    pub fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "UnitItem<name: {}, id: {}, value: {}, type: {}>",
+            self.name, self.unit_id, self.value, self.unit_type
+        ))
+    }
+
+    pub fn is_on(&self) -> bool {
+        self.value == 100
+    }
+}
+
+impl UnitItem {
+    pub fn with_value(mut self, value: i32) -> Self {
+        self.value = value;
+        return self;
+    }
+}
 
 #[pymethods]
 impl PyCuClient {
@@ -36,6 +67,7 @@ impl PyCuClient {
             Ok(PyCuClient(Arc::new(Mutex::new(client))))
         })
     }
+
     pub fn request<'p>(&mut self, py: Python<'p>, request: String) -> PyResult<&'p PyAny> {
         let client = Arc::clone(&self.0);
         pyo3_asyncio::async_std::future_into_py(py, async move {
@@ -44,6 +76,65 @@ impl PyCuClient {
                 Err(e) => return Err(CombinedErrorWrapper(e).into()),
             })
         })
+    }
+
+    pub fn get_all_items<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
+        let client = Arc::clone(&self.0);
+        pyo3_asyncio::async_std::future_into_py(py, async move {
+            let resp = match client.lock().await.get_all().await {
+                Ok(v) => v,
+                Err(e) => return Err(CombinedErrorWrapper(e).into()),
+            };
+            let mut result: Vec<UnitItem> = Vec::new();
+            let places = match resp.place {
+                Some(v) => v,
+                None => return Ok(result),
+            };
+            for zone in &places.zones {
+                for item in &zone.items {
+                    result.push(UnitItem {
+                        zone: zone.name.to_owned(),
+                        name: item.name.to_owned(),
+                        unit_id: item.unit_id,
+                        value: item.value,
+                        unit_type: item.unit_type,
+                    })
+                }
+            }
+            Ok(result)
+        })
+    }
+
+    pub fn change_state<'p>(
+        &mut self,
+        py: Python<'p>,
+        item: UnitItem,
+        new_state: i32,
+    ) -> PyResult<&'p PyAny> {
+        let client = Arc::clone(&self.0);
+        pyo3_asyncio::async_std::future_into_py(py, async move {
+            match client
+                .lock()
+                .await
+                .unit_operation(&UnitItemOperation {
+                    new_state,
+                    unit_type: item.unit_type,
+                    unit_id: item.unit_id,
+                })
+                .await
+            {
+                Ok(_) => Ok(item.clone().with_value(new_state)),
+                Err(e) => return Err(CombinedErrorWrapper(e).into()),
+            }
+        })
+    }
+
+    pub fn turn_on<'p>(&mut self, py: Python<'p>, item: UnitItem) -> PyResult<&'p PyAny> {
+        self.change_state(py, item, 100)
+    }
+
+    pub fn turn_off<'p>(&mut self, py: Python<'p>, item: UnitItem) -> PyResult<&'p PyAny> {
+        self.change_state(py, item, 0)
     }
 }
 
